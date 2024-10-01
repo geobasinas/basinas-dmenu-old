28b1f3d8e53e6c9247d8d4b04d4d363dc3fbc74a
basinasgit <79799018+geobasinas@users.noreply.github.com>
2024-10-01 20:30:37 +0300

    init

---
+++ config.def.h
@@ -0,0 +1,35 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ECEFF4", "#4C566A" },
+    [SchemeSel] = { "#ECEFF4", "#81A1C1" },
+    [SchemeSelHighlight] = { "#D8DEE9", "#81A1C1" },
+    [SchemeNormHighlight] = { "#D8DEE9", "#81A1C1" },
+    [SchemeOut] = { "#4C566A", "#8FBCBB" },
+    [SchemeMid] = { "#ECEFF4", "#4C566A" },
+
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
